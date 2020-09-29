1ae9f17f410a2684aa17846fabfe1f844a52ee4e
marysoloman <nikitamarysolomanpvt@gmail.com>
2020-09-29 10:49:16 +0530

    first commit- code format updated cmd+option+L

--- app/src/main/java/ca/yorku/cse/mack/fittstilt/Circle.java
+++ app/src/main/java/ca/yorku/cse/mack/fittstilt/Circle.java
@@ -4,45 +4,40 @@ import android.util.Log;
 
 import static java.lang.Math.abs;
 
-public class Circle
-{
-	final static int NORMAL = 1;
-	final static int TARGET = 2;
-	
-	public float x, y, radius;
-	int status; // indicates if this circle is the target circle or a normal circle
+public class Circle {
+    final static int NORMAL = 1;
+    final static int TARGET = 2;
 
-	Circle(float xArg, float yArg, float radiusArg, int statusArg)
-	{
-		x = xArg;
-		y = yArg;
-		radius = radiusArg;
-		status = statusArg;
-	}
-	
-	public boolean inCircle(float xTest, float yTest, float diameterArg)
-	{
-		float dx = xTest - x;
-		float dy = yTest - y;
-		float distanceFromCenter = (float)Math.sqrt(dx * dx + dy * dy);
-		return distanceFromCenter + diameterArg / 2f < radius;
-	}
+    public float x, y, radius;
+    int status; // indicates if this circle is the target circle or a normal circle
 
-    public boolean collidedWithCircle(float xCurrent, float yCurrent, float xPrev, float yPrev, float diameterArg){
+    Circle(float xArg, float yArg, float radiusArg, int statusArg) {
+        x = xArg;
+        y = yArg;
+        radius = radiusArg;
+        status = statusArg;
+    }
+
+    public boolean inCircle(float xTest, float yTest, float diameterArg) {
+        float dx = xTest - x;
+        float dy = yTest - y;
+        float distanceFromCenter = (float) Math.sqrt(dx * dx + dy * dy);
+        return distanceFromCenter + diameterArg / 2f < radius;
+    }
+
+    public boolean collidedWithCircle(float xCurrent, float yCurrent, float xPrev, float yPrev, float diameterArg) {
         float dx = xCurrent - x;
         float dy = yCurrent - y;
         float dxCurrentPrev = xPrev - x;
         float dyCurrentPrev = yPrev - y;
-        float distanceFromCenter = (float)Math.sqrt(dx * dx + dy * dy);
-        float distanceFromCenterOld = (float)Math.sqrt(dxCurrentPrev * dxCurrentPrev + dyCurrentPrev * dyCurrentPrev);
-        if(distanceFromCenter > distanceFromCenterOld
+        float distanceFromCenter = (float) Math.sqrt(dx * dx + dy * dy);
+        float distanceFromCenterOld = (float) Math.sqrt(dxCurrentPrev * dxCurrentPrev + dyCurrentPrev * dyCurrentPrev);
+        if (distanceFromCenter > distanceFromCenterOld
                 &&
-                (distanceFromCenterOld < radius*1.5 ))
-        {
-                return true;
-        }
-        else{
-	        return false;
+                (distanceFromCenterOld < radius * 1.5)) {
+            return true;
+        } else {
+            return false;
         }
     }
 }
--- app/src/main/java/ca/yorku/cse/mack/fittstilt/TracePoint.java
+++ app/src/main/java/ca/yorku/cse/mack/fittstilt/TracePoint.java
@@ -1,14 +1,13 @@
 package ca.yorku.cse.mack.fittstilt;
 
-class TracePoint
-{
-	int x, y, t;
-	float tilt;
-	TracePoint(int xArg, int yArg, int tArg, float tiltArg)
-	{
-		x = xArg;
-		y = yArg;
-		t = tArg;
-		tilt = tiltArg;
-	}
+class TracePoint {
+    int x, y, t;
+    float tilt;
+
+    TracePoint(int xArg, int yArg, int tArg, float tiltArg) {
+        x = xArg;
+        y = yArg;
+        t = tArg;
+        tilt = tiltArg;
+    }
 }
